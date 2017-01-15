b7363375771d4b3bce9de6f5b77ea851dcacdf56
Núbio Knupp <nubioknupp@gmail.com>
2017-01-15 13:11:57 -0200

    Crie um método para salvar as anotações no local storage.

--- MinhasAnotacoes/MinhasAnotacoes/ViewController.swift
+++ MinhasAnotacoes/MinhasAnotacoes/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var anotacoesTextView: UITextView!;
+    
+    let minhasAnotacoesChave = "MinhasAnotacoes";
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let myColor = UIColor(red: 65.0/255.0, green: 52.0/255.0, blue: 46.0/255.0, alpha: 1.0);
-        
         self.navigationController?.navigationBar.barTintColor = myColor;
         self.navigationController?.navigationBar.barStyle = UIBarStyle.black;
         
+       
+        let anotacoes = UserDefaults.standard.object(forKey: minhasAnotacoesChave);
         
+        anotacoesTextView.text = anotacoes as! String!;
         
     }
 
@@ -28,6 +34,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func SalvaAnotacoes(_ sender: Any) {
+        UserDefaults.standard.set(anotacoesTextView.text, forKey: minhasAnotacoesChave);
+    }    
+    
+    //ocultar a barra de status
+    override var prefersStatusBarHidden: Bool {
+        return true;
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true);
+    }
 }
 
